065f2e2ffe89759369d34fb0715a0862c211c142
ldva17 <99326703+ldva17@users.noreply.github.com>
2022-12-23 00:17:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({reset:true});
+sr.reveal('.he', {duration: 2000});  
+sr.reveal('.ac', {duration: 2000});  
+sr.reveal('.s2', {duration: 2000});
+sr.reveal('.ex', {duration: 2000}); 
+sr.reveal('.ha', {duration: 2000}); 
+sr.reveal('.op', {duration: 2000}); 
+sr.reveal('.d1', {duration: 4000}); 
+sr.reveal('.co', {duration: 4000});   
+sr.reveal('.haa', {duration: 4000});  
+sr.reveal('.coo', {duration: 4000}); 
+sr.reveal('.yy', {duration: 4000});
